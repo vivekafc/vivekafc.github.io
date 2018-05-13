11367208a6f0837afd384ca1e665c9a617860157
Vivek <bajav574@newschool.edu>
2018-05-13 16:35:05 -0400

    asgf
    dsgadg

--- lahainecolor/vinz/app.js
+++ lahainecolor/vinz/app.js
@@ -1,15 +1,5 @@
-var url = "https://raw.githubusercontent.com/vivekafc/coreinteraction/master/homework/lahaine/color.json";
-
-$.getJSON(url, function(data){
-    console.log(data.vinz);
-    //data.mycollection.sort();
-
-    for (var i = 0; i < data.vinz.length; i++) {
-
-       
-        $("#app").append("<div class='aclass'><div>new class</div><img src='" + data.vinz[i].image + "' ></div> ")
-        // where is says "app" --> you can attach it to anything like any div to have the info show up in a specific area
-
-    }
-})
-
+var $target = $('.wrapper');
+inView('.section').on('enter', function(el){
+  var color = $(el).attr('data-background-color');
+  $target.css('background-color', color );
+});
