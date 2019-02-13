211c96c9b9c1cebcbe591ed9418cb7fb9f11ff07
Vivek <bajav574@newschool.edu>
2019-02-13 04:50:17 -0500

    Update

---
+++ unikids/assets/js/app.js
@@ -0,0 +1,95 @@
+
+$(document).ready(function(){
+  // Add smooth scrolling to all links
+  $("a").on('click', function(event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+   
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
+});
+
+
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('h4')) {
+
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
+
+function handle(delta) {
+    var animationInterval = 20; //lower is faster
+  var scrollSpeed = 20; //lower is faster
+
+    if (end == null) {
+    end = $(window).scrollTop();
+  }
+  end -= 20 * delta;
+  goUp = delta > 0;
+
+  if (interval == null) {
+    interval = setInterval(function () {
+      var scrollTop = $(window).scrollTop();
+      var step = Math.round((end - scrollTop) / scrollSpeed);
+      if (scrollTop <= 0 || 
+          scrollTop >= $(window).prop("scrollHeight") - $(window).height() ||
+          goUp && step > -1 || 
+          !goUp && step < 1 ) {
+        clearInterval(interval);
+        interval = null;
+        end = null;
+      }
+      $(window).scrollTop(scrollTop + step );
+    }, animationInterval);
+  }
+}
+
+
+document.body.className += ' fade-out';
+$(function() {
+    $('body').removeClass('fade-out');
+});
+
+ScrollReveal().reveal('h1');
+ScrollReveal().reveal('h2');
+ScrollReveal().reveal('h6');
+ScrollReveal().reveal('p');
+ScrollReveal().reveal('left');
+ScrollReveal().reveal('left1');
+ScrollReveal().reveal('left2');
+ScrollReveal().reveal('left3');
+ScrollReveal().reveal('left4');
+ScrollReveal().reveal('right');
+ScrollReveal().reveal('right1');
+ScrollReveal().reveal('right2');
+ScrollReveal().reveal('right3');
+ScrollReveal().reveal('right4');
