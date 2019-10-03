d7ce0921e340802cdb1c43a15d5e359f9442dc19
Vivek <bajav574@newschool.edu>
2019-10-03 18:51:33 -0400

    5
    5

---
+++ excercise5/js/scripts.js
@@ -0,0 +1,84 @@
+var body = document.body;
+
+/*function boldFunction(){
+
+document.getElementById("name").style.color = "blue";
+document.getElementById("name").style.fontFamily = "Arial";
+document.getElementById("name").style.fontSize = "larger";
+document.getElementById("body").style.background = "grey";
+	
+	}*/
+
+
+
+var clock = document.getElementById("name");
+
+function colourFunction() 
+{
+    var r = function () { return Math.floor(Math.random()*256) };
+    return "rgb(" + r() + "," + r() + "," + r() + ")";
+}
+
+var colourFunc = function colourFunction() 
+{
+    var r = function () { return Math.floor(Math.random()*256) };
+    return "rgb(" + r() + "," + r() + "," + r() + ")";
+}
+
+function sizeFunction(){
+
+	var checkSize = document.getElementById("size").value;
+
+	clock.style.fontSize = checkSize + "px";
+	clock.style.color = colourFunc;
+
+}
+
+$('p').click(function(){ 
+	
+	var checkSize = document.getElementById("size").value;
+
+	clock.style.fontSize = checkSize + "px";
+
+})
+
+$('p').click(function(){ 
+	
+function colourFunction() 
+{
+    var r = function () { return Math.floor(Math.random()*256) };
+    return "rgb(" + r() + "," + r() + "," + r() + ")";
+}
+
+})
+
+
+
+$('p').click(function(){
+
+	function randLetter() { 
+    var letters = ["bajk", "bvek", "bajak", "bajek", "bajajivek", "vibajaj", "vivejaj", "vijaj", "bavivek", "bajvivek", "bajavivek"];
+    var name = letters[Math.floor(Math.random() * letters.length)];
+    return name
+}
+
+$('p').html(randLetter());})
+
+$('body').click(function(){
+
+	function bgChange() { 
+    var colours = ["red", "blue", "green", "yellow"];
+    var colour = colours[Math.floor(Math.random() * colours.length)];
+    return colour
+}
+
+$('body').style.background(bgChange());
+
+
+})
+
+
+
+
+$('p').trigger("click")
+
