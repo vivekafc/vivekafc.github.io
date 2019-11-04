5899ba13ed1d68e7d23267aff49f4e82904032d5
Vivek <bajav574@newschool.edu>
2019-11-04 18:24:07 -0500

    sofinaoisf
    widndas

--- interface/js/bpm.js
+++ interface/js/bpm.js
@@ -2,7 +2,7 @@ let cnv, soundFile, fft, peakDetect;
 let ellipseWidth = 10;
 
 function preload() {
-  soundFile = loadSound("wind.mp3");
+  soundFile = loadSound("cowbell.mp3");
 }
 
 function setup() {
