02943bc474a752c5dfc05734c66ba4e8af7b307d
Vivek <bajav574@newschool.edu>
2019-11-16 21:47:17 -0500

    insomnia
    insomnia

--- interface/js/scripts.js
+++ interface/js/scripts.js
@@ -1,36 +1,61 @@
+
+document.addEventListener("mousemove", function (event){
+
+  const x = event.clientX - 25
+  const y = event.clientY - 25
+
+  const cursor = document.querySelector("#cursor")
+
+  cursor.style.left = x + "px"
+  cursor.style.top = y  + "px" 
+
+
+})
+
+var canvas = document.querySelector('canvas');
+
+var musix = new Array("1.mp3", "2.mp3", "3.mp3", "4.mp3", "5.mp3", "6.mp3", "7.mp3", "8.mp3", "9.mp3", "10.mp3");
+
+var randomizer = musix[Math.floor(Math.random() * musix.length)];
+
 function preload(){
-  sound = loadSound('wind.mp3');
+  sound = loadSound(randomizer);
+
 }
 
 function setup(){
   let cnv = document.querySelector('canvas');
   cnv.mouseClicked(togglePlay);
-  var fft = new p5.FFT();
+  fft = new p5.FFT();
   sound.amp(0.2);
 }
 
 
 function generate (){var canvas = document.querySelector('canvas');
+
 var context = canvas.getContext('2d');
 
+fft = new p5.FFT();
 let waveform = fft.waveform();
+
+console.log(waveform);
 
 var size = window.innerWidth;
 var dpr = window.devicePixelRatio;
 canvas.width = size * dpr;
 canvas.height = size * dpr;
 context.scale(dpr, dpr); 
-context.lineWidth = Math.floor(Math.random() * 4) + 1;
+context.lineWidth = waveform;
 
 
-var step = Math.floor(Math.random() * 10) + 5  ;
+var step = Math.floor(Math.random(waveform) * 10)  ;
 var lines = [];
 
 for(var i = step; i <= size - step; i += step) {
     
   var line = [];
   for(var j = step; j <= size - step; j+= step) {
-    var random = Math.random() * 10;
+    var random = Math.random(waveform) * 10;
     var point = {x: j, y: i + random};
     line.push(point);
   } 
@@ -46,8 +71,10 @@ for(var i = 0; i < lines.length; i++) {
     context.lineTo(lines[i][j].x, lines[i][j].y);
   }
 
-context.strokeStyle = Math.floor(Math.random() * 256);
+context.strokeStyle = Math.floor(Math.random(waveform) * 256);
   context.stroke();
+
+
 }
 
 
@@ -64,3 +91,23 @@ function random_bg_color() {
 random_bg_color();}
 
 generate();
+
+
+function play(){
+
+       var audio = sound;
+       audio.play();
+
+
+                 }
+
+function reload(){
+  window.location.reload(true);
+}
+
+
+  document.querySelector('canvas').onload="play()"
+
+
+
+
