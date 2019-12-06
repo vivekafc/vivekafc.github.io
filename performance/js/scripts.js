db91c28d544c03000dfc5ac9d332e090c759732e
Vivek <bajav574@newschool.edu>
2019-12-05 20:18:37 -0500

    ofh
    sdgasd

---
+++ performance/js/scripts.js
@@ -0,0 +1,24 @@
+$('.moon').click(function(){ 
+	
+	$('.moon').css('top', '100px');
+	$('.moon').css('bottom', '100px');
+	$('.moon').css('left', '100px');
+	$('.moon').css('right', '1000px');
+
+	setInterval(function(){
+
+		console.log();
+
+		var randomNumber = Math.random() * 500 + "px";
+
+		$('.moon').css('top', randomNumber);
+		$('.moon').css('bottom', randomNumber);
+		$('.moon').css('left', randomNumber);
+		$('.moon').css('right', randomNumber);
+
+	}, 1000)
+
+})
+
+
+$('.moon').trigger("click")
